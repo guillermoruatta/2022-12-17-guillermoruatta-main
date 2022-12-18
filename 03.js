ba62abe5fb3bef2a04bb673ee69e0a5170fda647
vjpacks <marianasajen29@gmail.com>
2022-12-18 03:28:11 -0300

    yes

--- 03.js
+++ 03.js
@@ -1,28 +1,16 @@
-/* 
-  Importante: 
-  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
-  código dentro de las funciones ya definidas. 
-  No comentar la funcion 
-*/
- {
-  // La funcion llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene
-  // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También
-  // recibe un string llamado 'nombre'.
-  // Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre' recibido por argumento.
-  // Ej:
-  //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
-  //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
-  // Tu código aca:
+ {
+ 
+
   function buscarAmigo(amigos, nombre) {
-    // Iterate through the array of objects and find the one with a matching name
+    
     for (let i = 0; i < amigos.length; i++) {
       if (amigos[i].nombre === nombre) {
-        // Return the object if a match is found
+       
         return amigos[i];
       }
     }
-    // Return null if no match is found
+    
     return null;
   }
   
@@ -30,6 +18,6 @@
   
 }
 
-// No modifiques nada debajo de esta linea //
+
 
 module.exports = buscarAmigo
--- 06-07-08.js
+++ 06-07-08.js
@@ -1,16 +1,4 @@
-/* 
-  Importante: 
-  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
-  código dentro de las funciones ya definidas. 
-  No comentar la funcion 
-*/
-// function crearClasePersona() {
-  // class Persona {
-   // constructor(nombre, edad, hobbies, amigos) {
-      // El constructor de la clase Persona recibe nombre (string), edad (integer), hobbies (array de strings), amigos (array de objetos)
-      // Inicializar las propiedades de la persona con los valores recibidos como argumento
 
-      // Tu código aca:
       {
       constructor(nombre, edad, hobbies, amigos) 
         this.nombre = nombre;
@@ -19,36 +7,20 @@
         this.amigos = amigos;
 }
   
-    // addFriend(nombre, edad) {
-      // El método 'addFriend' recibe un string 'nombre' y un entero 'edad' y debe agregar un objeto:
-      // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
-      // No debe retornar nada.
-
-      // Tu código aca:
+ 
      {
       addFriend(nombre, edad) 
         this.amigos.push({ nombre: nombre, edad: edad });
       }
       
 
-    // addHobby(hobby) {
-      // El método 'addHobby' recibe un string 'hobby' y debe agregarlo al arreglo de hobbies de la persona.
-      // No debe retornar nada.
-
-      // Tu código aca:
+  
       {
         addHobby(hobby)
         this.hobbies.push(hobby);
       }
     
-    // getFriends() {
-      // El método 'getFriends' debe retornar un arreglo con sólo los nombres del arreglo de amigos
-      // de la persona.
-      // Ej:
-      // Suponiendo que la persona tiene estos amigos: [{nombre: 'martin', edad: 31},{nombre: 'toni', edad: 33}]
-      // persona.getFriends() debería devolver ['martin', 'toni']
-
-      // Tu código aca:
+    
        {
         return this.amigos.map(amigo => amigo.nombre);
       }
@@ -60,12 +32,7 @@
       
   
 
-    // getHobbies() {
-      // El método 'getHobbies' debe retornar un arreglo con los hobbies de la persona
-      // Ej:
-      // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
-
-      // Tu código aca:
+    
       {
             getHobbies() 
         return this.hobbies;
@@ -73,22 +40,7 @@
       
     
 
-   // getPromedioEdad() {
-      // El método 'getPromedioEdad' debe retornar el promedio de edad de los amigos de una persona
-      // Ej:
-      // Si la persona tuviera estos amigos:
-      // {
-      //   amigos: [{
-      //     nombre: 'toni',
-      //     edad: 33,
-      //   }, {
-      //     nombre: 'Emi',
-      //     edad: 25
-      //   }]
-      // }
-      // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
-
-      // Tu código aca:
+  
 
       {
       getPromedioEdad() 
@@ -101,6 +53,5 @@
       
   
 
-// No modifiques nada debajo de esta linea //
 
 module.exports = crearClasePersona
